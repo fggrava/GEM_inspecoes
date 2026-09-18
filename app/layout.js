d8887d2193af485218db403fbe7da6b0a44a3dc2
fggrava <fg.grava@gmail.com>
2026-09-18 04:55:22 -0300

    Adiciona layout principal

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"GEM Inspeções",description:"Gestão Estratégica de Manutenção"};
+export default function RootLayout({children}){return <html lang="pt-BR"><body>{children}</body></html>}
